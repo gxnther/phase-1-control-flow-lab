5a1377462a06eedeaff36fdfb449accaee7de557
Gunther <gunt.valentine@gmail.com>
2022-03-06 17:16:51 -0500

    commit tree 4129fa8d4edd613e661b981e93c7edcda34b4a2c

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+ if (distance <= 400) {
+   return 'This one is on me!' ; 
+ } if (distance >= 2000 && distance <= 2500) {
+   return 'I will gladly take your thirty bucks.'
+ } if (distance >= 2500) {
+   return `No can do.`
+ }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city) {
+  return (city === "NYC") ?  "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+ternaryCheckCity(NYC);
+ 
+
+function switchOnCharmFromTip(tipped) {
+  if (tipped === "generous") {
+    return "Thank you so much." 
+  } if (tipped === `not as generous`) {
+  return "Thank you." 
+  } if (tipped === "thanks for everything") {
+    return "Bye."
 }
+}
+switchOnCharmFromTip(tipped);
